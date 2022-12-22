f33db25a3ee801b99c4cd8c51341a822cf5d4bc0
Samuel Rus <97249183+ProSureString@users.noreply.github.com>
2022-12-22 14:58:41 -0800

    MuchSimple

--- main.ts
+++ main.ts
@@ -1,24 +1,14 @@
-function Calc () {
-    num1 = game.askForNumber("num1")
-    num2 = game.askForNumber("num2")
-    opertor = game.askForString("The operator a+ s- m* d/")
-    if (opertor == "A" || opertor == "a") {
-        out = num1 + num2
-    } else if (opertor == "S" || opertor == "s") {
-        out = num1 - num2
-    } else if (opertor == "M" || opertor == "m") {
-        out = num1 * num2
-    } else if (opertor == "D" || opertor == "d") {
-        out = num1 / num2
-    }
-    game.splash(out)
-    callCalc()
-}
-function callCalc () {
-    Calc()
-}
 let out = 0
-let opertor = ""
-let num2 = 0
-let num1 = 0
-Calc()
+let num1 = game.askForNumber("num1")
+let num2 = game.askForNumber("num2")
+let opertor = game.askForString("The operator a+ s- m* d/")
+if (opertor == "A" || opertor == "a") {
+    out = num1 + num2
+} else if (opertor == "S" || opertor == "s") {
+    out = num1 - num2
+} else if (opertor == "M" || opertor == "m") {
+    out = num1 * num2
+} else if (opertor == "D" || opertor == "d") {
+    out = num1 / num2
+}
+game.splash(out)
