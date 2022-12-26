6ec2ecb30c0a319ca5ea87499ab1967975a0dfeb
Samuel Rus <97249183+ProSureString@users.noreply.github.com>
2022-12-26 10:21:32 -0800

    Much much better

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
 let out = 0
-game.splash("Welcome to calculator!!!", "Please reset after each calculation...")
 let num1 = game.askForNumber("num1")
 let num2 = game.askForNumber("num2")
-let opertor = game.askForString("The operator a+ s- m* d/")
+let opertor = game.askForString("The operator asmd")
 if (opertor == "A" || opertor == "a") {
     out = num1 + num2
 } else if (opertor == "S" || opertor == "s") {
